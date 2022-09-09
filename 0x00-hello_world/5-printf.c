efdee702abeae58e0c80352b8f19ca76da947636
dev-myk <devmikekenya@gmail.com>
2022-09-08 19:42:23 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammer, but the output is a piece of art,"
+ *
+ * Return: Always o (Success)
+ */
+
+int main(void)
+{printf("with proper grammer, but the outcome is a piece of art, \n");
+	return (o);
+}
