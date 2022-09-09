85a38c0c34baba250bb56daa8fd320937c2c4048
dev-myk <devmikekenya@gmail.com>
2022-09-08 19:47:38 -0700

    hot fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Prints "with proper grammer, but the output is a piece of art,"
+ * main - Prints "with proper grammer, but the ouptu is a piece of art,"
  *
  * Return: Always o (Success)
  */
 
 int main(void)
-{printf("with proper grammer, but the outcome is a piece of art, \n");
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (o);
 }
