e2e825eae6114a95af65cffbdc1f283d5eea3ebc
dev-myk <devmikekenya@gmail.com>
2022-09-09 22:00:05 -0700

    hot fix

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
- * main - main block
- * Description: print all possible combinations of two digits.
- * numbers must be separated by commas and a space.
- * 01 and 10 are considered as the same combination of the two digits.
- * print only the smallest combination of two digits.
- * numbers should be printed in ascending order, with two digits.
- * you can only use 'putchar'.
- * you can only use 'putchar' up to 5 times.
- * you are not allowed to use any variable of type 'char'.
- */
+* main - main block
+* Description: print all possible combinations of two digits.
+* numbers must be separated by commas and a space.
+* 01 and 10 are considered as the same combination of the two digits.
+* print only the smallest combination of two digits.
+* numbers should be printed in ascending order, with two digits.
+* you can only use 'putchar'.
+* you can only use 'putchar' up to 5 times.
+* you are not allowed to use any variable of type 'char'.
+*/
 int main(void)
 {
 	int i, j, k;
